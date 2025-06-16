fc870e2f4b56b7de645b661f0e2bb9b631f2e7c0
vovka8q5 <vovka.sh.85@gmail.com>
2025-06-16 22:51:27 +0300

    Delete telegram_bot.py

--- telegram_bot.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-
-def send_telegram_alert(message):
-    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    requests.post(url, json={"chat_id": chat_id, "text": message})
