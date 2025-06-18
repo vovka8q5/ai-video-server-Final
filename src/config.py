ab2f65e12a929cc0dc8c58f73ca60bc1b9322a66
vovka8q5 <vovka.sh.85@gmail.com>
2025-06-18 23:46:29 +0300

    config.py

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 # Путь к файлам
 CLIENT_SECRETS_FILE = "/etc/secrets/client_secrets.json"
-TOKEN_FILE = "/etc/secrets/token.pickle"  # Обновленный путь
+TOKEN_FILE = "/etc/secrets/token.pickle"
 VIDEOS_DIR = "videos/"
 FACES_DIR = "faces/"
 MUSIC_PATH = "music.mp3"
